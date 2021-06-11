57fe71f3852b23952af79ab3335a729ab2c67c2b
thitthitzaw <thitthitzaw@global-connect.asia>
2021-06-11 22:22:18 +0630

    is it new year

--- lecture3/newyear/views.py
+++ lecture3/newyear/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from datetime import datetime
 
 # Create your views here.
+def index(request):
+    today = datetime.now()
+    values = {
+        "newyear": datetime.now().day == 1 and datetime.month == 1
+    }
+    return render(request,"newyear/index.html",values)
